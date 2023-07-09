d1a3dc96ea3acf6dba9b29b482290efa9628128f
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2023-07-09 20:40:34 +0700

    update gameloop

---
+++ Assets/_Projects/Scripts/Managers/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using GMTK.Fisherman;
+
+namespace GMTK.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private float maxStackMoodTimer = 2f;
+
+        private float currentStackMoodTimer = 0f;
+
+        private void Update()
+        {
+            if (Fisher.Instance.currentMood == Fisher.Instance.minMood || Fisher.Instance.currentMood == Fisher.Instance.maxMood)
+            {
+                currentStackMoodTimer += Time.deltaTime;
+
+                if (currentStackMoodTimer >= maxStackMoodTimer)
+                {
+                    currentStackMoodTimer = 0f;
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }
+            }
+        }
+    }
+}
