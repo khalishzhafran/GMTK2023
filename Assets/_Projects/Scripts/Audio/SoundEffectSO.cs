c13433a00637ecdd0089896967cdc71d836ed7e4
Muhammad Khalish Zhafran <khalish260303@gmail.com>
2023-07-09 21:47:30 +0700

    update sound

---
+++ Assets/_Projects/Scripts/Audio/SoundEffectSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GMTK.Audio
+{
+    [CreateAssetMenu(menuName = "GMTK/Audio/SoundEffectSO")]
+    public class SoundEffectSO : ScriptableObject
+    {
+        public AudioClip ReelIn;
+        public AudioClip ReelInMiddle;
+        public AudioClip ReelInEnd;
+        public AudioClip ReelFailed;
+        public AudioClip AnglerReactionVarieties_Mad;
+        public AudioClip AnglerReactionVarieties_Happy;
+        public AudioClip AcceptButton;
+        public AudioClip BackButton;
+        public AudioClip MoodPointGained;
+    }
+}
